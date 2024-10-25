4539d8dd8e730c7523cc0b9865bc504da982cbc6
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-10-25 17:00:22 +0200

    feat: implementation toCsvText function (Fri, Okt 25, 2024 17:00:12 PM)

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -199,8 +199,9 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  const newArr = arr.map((el, i) => (i < arr.length - 1 ? `${el.join(',')}\n` : `${el.join(',')}`));
+  return newArr.join('');
 }
 
 /**
